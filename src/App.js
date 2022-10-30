33b07548ef7cd08954925b9ca6492da55dfda690
Marcelo Fonseca <marcelofonseca02@outlook.com>
2022-10-29 23:11:30 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './componentes/Logo'
+import perfil from './imagens/perfil.png'
+import carrinho from './imagens/carrinho.png'
+import sino from './imagens/sino.png'
+
+const icones= [ carrinho, sino, perfil ]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Logo></Logo>
+      
+      <ul className="icones">
+        { icones.map( (icone) => (
+          <li className="icone"><img src={ icone } alt="icone"></img></li>
+        )) }
+      </ul>
       </header>
+     
     </div>
   );
 }
---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,13 @@
+import './estilo.css'
+import logo from '../../imagens/logo.png'
+
+function Logo(){
+    return(
+        <div className="logo">
+        <img src={logo} alt="logo" className="logo-img"/>
+
+      </div>
+    )
+}
+
+export default Logo
